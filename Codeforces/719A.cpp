087696695617c9afc4950395ec6ea1e018956655
GLEDSONS831 <glegogle84@gmail.com>
2019-09-29 20:00:51 -0300

    Codeforces/feature: Accepted

--- Codeforces/719A.cpp
+++ Codeforces/719A.cpp
@@ -3,11 +3,18 @@
 using namespace std;
 
 int main(){
-    int a, b;
-    cin>>a>>b;
-    int years = 0;
-    while(a <= b){
-        years++; a *= 3; b *= 2;
+    int n;
+    cin>>n;
+    int bills = 0;
+    int l[5] = {100, 20, 10, 5, 1};
+    int i = 0;
+    while(i < 5){
+        if(n % l[i] == 0){
+            bills++;
+        }
+        else{
+            n /= l[i++];
+        }
     }
-    cout<<years<<endl;
+    cout<<bills<<endl;
 }
