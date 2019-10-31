c00b9fbfc126cc6cf0c2700564a8d61b3dea79c1
GLEDSONS831 <glegogle84@gmail.com>
2019-10-31 12:03:03 -0300

    Codeforces/feature: :sparkles: Accepted

--- Codeforces/614A.cpp
+++ Codeforces/614A.cpp
@@ -1,39 +1,33 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-long double num1(unsigned long long base, unsigned long long lg){
-    return log10(lg) / log10(base);
-}
-
-unsigned long long conv(long double n){
-    if(n - (unsigned long long)n == 0)return (unsigned long long )n;
-    else return ((unsigned long long)n+1);
-
-}
-
+ 
+#define ll long long
+ 
 int main(){
-    unsigned long long l, r, k; cin >> l >> r >> k;
-    long double  first = num1(k, l);
-    unsigned long long first_power = conv(first);
-
-    if(pow(k, first_power) > r){
-        cout << -1 << endl;
-    }
-    else{
-        unsigned long long count = 1; 
-        while(1){
-            
-            if(count >= l){
-                if(count * k > r) cout << count <<endl;
-                else cout << count << " ";
-            }
-            count *= k;
-            if(count > r){
-                break;
-            }
-            
+    ll l, r, k, ant; 
+   
+    cin >> l >> r >> k;
+    vector < ll > vc;
+    ll aux = 1;
+ 
+    bool has = false;
+    while(1){
+        if( aux >= l){
+           vc.push_back(aux);
+            has = true;
         }
+        if( (r / k) < aux){
+            break;
+        }
+        aux *= k;
     }
-    
+    if( not has ) cout << -1 << endl;
+    else{
+      ll i = 0;
+      for(i = 0; i < vc.size()-1; i++){
+        cout << vc[i] << " ";
+      }
+      cout << vc[i] << endl;
+    }
 }
